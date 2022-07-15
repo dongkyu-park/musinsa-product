b81ce42e973d5b5eca84d400b0ed705b7c514b72
dongkyu <dongkyu.parkk@gmail.com>
2022-07-15 23:14:02 +0900

    feat: 더미 데이터 추가

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,87 @@
+---- 상의
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'A', '11200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'B', '10500', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'C', '10000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'D', '10100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'E', '10700', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'F', '11200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'G', '10500', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'H', '10800', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('상의', 'I', '11400', now(), now(), 0);
+
+---- 아우터
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'A', '5500', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'B', '5900', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'C', '6200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'D', '5100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'E', '5000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'F', '7200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'G', '5800', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'H', '6300', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('아우터', 'I', '6700', now(), now(), 0);
+
+---- 바지
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'A', '4200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'B', '3800', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'C', '3300', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'D', '3000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'E', '3800', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'F', '4000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'G', '3900', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'H', '3100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('바지', 'I', '3200', now(), now(), 0);
+
+---- 스니커즈
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'A', '9000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'B', '9100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'C', '9200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'D', '9500', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'E', '9900', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'F', '9300', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'G', '9000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'H', '9700', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('스니커즈', 'I', '9500', now(), now(), 0);
+
+---- 가방
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'A', '2000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'B', '2100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'C', '2200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'D', '2500', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'E', '2300', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'F', '2100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'G', '2200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'H', '2100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('가방', 'I', '2400', now(), now(), 0);
+
+---- 모자
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'A', '1700', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'B', '2000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'C', '1900', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'D', '1500', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'E', '1800', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'F', '1600', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'G', '1700', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'H', '1600', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('모자', 'I', '1700', now(), now(), 0);
+
+---- 양말
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'A', '1800', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'B', '2000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'C', '2200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'D', '2400', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'E', '2100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'F', '2300', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'G', '2100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'H', '2000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('양말', 'I', '1700', now(), now(), 0);
+
+---- 액세서리
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'A', '2300', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'B', '2200', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'C', '2100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'D', '2000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'E', '2100', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'F', '1900', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'G', '2000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'H', '2000', now(), now(), 0);
+insert into product(type, brand, price, created_at, updated_at, is_deleted) values ('액세서리', 'I', '2400', now(), now(), 0);
